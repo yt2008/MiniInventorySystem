c3a89bc037d9c34f7b1bfd4d573054ad787daf25
DESKTOP\Yatong Zhou <mike.z@anzuk.education>
2021-01-06 23:14:50 +1100

    Add project files.

---
+++ MiniInventorySystem/Models/Computer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniInventorySystem.Models
+{
+    public class Computer
+    {
+        public Guid Id { get; set; }
+        public string Processor { get; set; }
+        public string Brand { get; set; }
+        public int UsbPorts { get; set; }
+        public int RamSlots { get; set; }
+        public string FromFactor { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
